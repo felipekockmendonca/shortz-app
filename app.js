9444c382757e3c6e56465fdc7f55240ac845d02a
Felipe Kock Mendonça <kockmendoncafelipe@gmail.com>
2026-03-24 19:18:20 -0300

    Novo Repositorio

---
+++ app.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'frase_secreta_aqui',   // mude isso!
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 dia
+}));
+app.use(flash());
+app.use((req, res, next) => { 
+  res.locals.messages = req.flash();
+  next();
+});
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Importa o objeto 'sequelize' para conexão com o banco de dados
+const sequelize = require('./config/database');
+// Importa o modelo User para sincronização
+const user = require('./modules/user/userModel');
+// Sincroniza o modelo com o banco de dados
+sequelize.sync({ alter: true })
+    .then(() => console.log('Banco de dados sincronizado!'))
+    .catch(err => console.error('Erro ao sincronizar banco:', err));
+
+module.exports = app;
